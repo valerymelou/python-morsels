1e93cb397d9717ee7e184f94f70b4520aa701a11
Valery Melou <valerymelou@gmail.com>
2018-05-03 22:28:17 +0100

    Improve flip_dict_of_lists function

--- flip/flip.py
+++ flip/flip.py
@@ -1,4 +1,4 @@
-def flip_dict_of_lists(dict, dict_type=None, key_func=None):
+def flip_dict_of_lists(dict_of_lists, dict_type=None, key_func=lambda k: k):
     """
     Returns a "flipped" dictionary of lists from the given dictionnary of lists.
     What I mean by "flipped" is this:
@@ -8,16 +8,13 @@ def flip_dict_of_lists(dict, dict_type=None, key_func=None):
     {1:  ['a', 'b'], 2: ['a', 'c'], 3: ['b']}
     """
     if dict_type is not None:
-        flipped_dict = dict_type()
+        flipped = dict_type()
     else:
-        flipped_dict = {}
-    for key in dict.keys():
-        for item in dict[key]:
-            if key_func is not None:
-                item = key_func(item)
-            if item in flipped_dict:
-                flipped_dict[item].append(key)
-            else:
-                flipped_dict[item] = [key]
+        flipped = {}
+    for old_key, old_values in dict_of_lists.items():
+        for old_value in old_values:
+            new_key = key_func(old_value)
+            new_value = old_key
+            flipped.setdefault(new_key, []).append(new_value)
     
-    return flipped_dict
+    return flipped
